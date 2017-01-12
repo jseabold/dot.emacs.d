1fcbb631d0d1fe331d893e634ea7e278292666f2
Skipper Seabold <jsseabold@gmail.com>
2017-01-12 08:16:47 -0600

    Initial commit

---
+++ init.el
@@ -0,0 +1,23 @@
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
+(add-to-list 'load-path (expand-file-name "package-config/" user-emacs-directory))
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+
+;; Package Configuration
+
+(require 'package)
+(package-initialize)
+
+ ;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+(use-package solarized-theme
+    :ensure t
+    :config (progn (load-theme 'solarized-dark t)))
+
+(require 'init-evil-mode)
