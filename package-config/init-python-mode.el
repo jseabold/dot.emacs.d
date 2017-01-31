81ec255fce024d00f0e6c8c02a176997f9a09641
Skipper Seabold <sseabold@civisanalytics.com>
2017-01-31 08:37:54 -0600

    Revert "Try using ipython 5 inferior shell"
    This reverts commit dbc81c7555becd09252e8b1b45b9710c42b918d8.

--- package-config/init-python-mode.el
+++ package-config/init-python-mode.el
@@ -1,15 +1,4 @@
-;; Emacs >= 24.3 supports ipython inferior shell out of the box but
-;; IPython >=5.0.0 has a new prompt technology that screws with Emacs.
-;; We fix it temporarily using its simple interface.
-(when (and (executable-find "ipython")
-           (version<= "24.3" emacs-version))
-
-  (setq python-shell-interpreter "ipython"
-        python-shell-interpreter-args "--simple-prompt --pprint"
-        python-shell-completion-native-enable nil
-        ))
-
-(use-package  anaconda-mode
+(use-package anaconda-mode
   :ensure t
   :init
   (progn
@@ -17,10 +6,5 @@
     (add-hook 'python-mode-hook 'jedi:setup)
     )
   )
-
-;; Keys
-; (with-eval-after-load "python"
-;   (define-key python-mode-map (kbd "<f10>") 'python-shell-switch-to-shell)
-;   (define-key inferior-python-mode-map (kbd "<f10>") 'delete-window))
 
 (provide 'init-python-mode)
