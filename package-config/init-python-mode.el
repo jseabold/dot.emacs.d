dbc81c7555becd09252e8b1b45b9710c42b918d8
Skipper Seabold <sseabold@civisanalytics.com>
2017-01-31 08:36:30 -0600

    Try using ipython 5 inferior shell

--- package-config/init-python-mode.el
+++ package-config/init-python-mode.el
@@ -1,4 +1,15 @@
-(use-package anaconda-mode
+;; Emacs >= 24.3 supports ipython inferior shell out of the box but
+;; IPython >=5.0.0 has a new prompt technology that screws with Emacs.
+;; We fix it temporarily using its simple interface.
+(when (and (executable-find "ipython")
+           (version<= "24.3" emacs-version))
+
+  (setq python-shell-interpreter "ipython"
+        python-shell-interpreter-args "--simple-prompt --pprint"
+        python-shell-completion-native-enable nil
+        ))
+
+(use-package  anaconda-mode
   :ensure t
   :init
   (progn
@@ -6,5 +17,10 @@
     (add-hook 'python-mode-hook 'jedi:setup)
     )
   )
+
+;; Keys
+; (with-eval-after-load "python"
+;   (define-key python-mode-map (kbd "<f10>") 'python-shell-switch-to-shell)
+;   (define-key inferior-python-mode-map (kbd "<f10>") 'delete-window))
 
 (provide 'init-python-mode)
