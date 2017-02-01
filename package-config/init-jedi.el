5b40cb41214737fa60d72ef2dd067b0ffe6f3986
Skipper Seabold <sseabold@civisanalytics.com>
2017-02-01 09:40:10 -0600

    Allow jedi to use global miniconda python

--- package-config/init-jedi.el
+++ package-config/init-jedi.el
@@ -1,9 +1,19 @@
 ;; First install dependencies
 ;; conda install -y -c asmeurer jedi sexpdata epc virtualenv
 ;; Then run M-x jedi:install-server
+;; If you want to use jedi from other environments, point to this installation
+;; explicitly
 
 (use-package jedi
   :ensure t
+  :config (progn
+            (setq debug-on-error t)
+            (setq jedi:server-command
+                  ;; ` indicates a backquoted/quasiquoted list
+                  ;; the comma indicates which part to evaluate
+                  `("~/.miniconda3/bin/python"
+                    ,(concat jedi:source-dir "jediepcserver.py")))
+            )
   )
 
 (provide 'init-jedi)
