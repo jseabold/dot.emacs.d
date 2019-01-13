b8bc9ff55a5fd02f513fb625d506fcec9b6e557e
Skipper Seabold <jsseabold@gmail.com>
2019-01-13 09:30:36 -0600

    Add js2 and jsx for react

---
+++ package-config/init-rjsx-mode.el
@@ -0,0 +1,11 @@
+(use-package rjsx-mode
+  :mode (("\\.js$" . rjsx-mode)
+         ))
+:config (progn
+          (setq-default js-indent-level 2
+                        js2-basic-indent 2
+                        js2-basic-offset 2
+                        js2-strict-trailing-comma-warning nil
+                        ))
+
+(provide 'init-rjsx-mode)
