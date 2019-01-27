d6811b5ab6ecfdea3e2330bae306d965195affa0
Skipper Seabold <jsseabold@gmail.com>
2019-01-27 08:54:09 -0600

    sane colors in helm w/ dark theme

--- package-config/init-helm.el
+++ package-config/init-helm.el
@@ -5,6 +5,16 @@
   (helm-mode 1)
   (setq helm-autoresize-mode t)
   (setq helm-buffer-max-length 40)
+  (set-face-attribute 'helm-selection nil
+                      :background "blue"
+                      :distant-foreground "white")
+  (set-face-attribute 'helm-match nil
+                      :background "gray"
+                      :foreground "black")
+  (set-face-attribute 'helm-buffer-process nil
+                      :foreground "white")
+  (set-face-attribute 'helm-buffer-size nil
+                      :foreground "white")
   ;; M-x is now helm-M-x
   (global-set-key (kbd "M-x") #'helm-M-x)
   ;; Go up a level inside helm with C-k
