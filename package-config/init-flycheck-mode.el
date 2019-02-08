27fefd5702a6a044558764e73027cf9dde260855
Skipper Seabold <sseabold@civisanalytics.com>
2019-02-08 12:48:00 -0600

    Setup flycheck

--- package-config/init-flycheck-mode.el
+++ package-config/init-flycheck-mode.el
@@ -3,6 +3,21 @@
   :diminish flycheck-mode
   :config
   (global-flycheck-mode)
+  (setq-default flycheck-disabled-checkets
+                '(javascript-jshint))
+  (setq flycheck-checkers '(emacs-lisp
+                            javascript-eslint
+                            sass
+                             ))
+  (setq flycheck-highlighting-mode 'lines)
+  (setq flycheck-indication-mode 'right-fringe)
   )
+
+(use-package flycheck-color-mode-line
+             :ensure t
+             :diminish flycheck-color-mode-line
+             :config
+             (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)
+             )
 
 (provide 'init-flycheck-mode)
