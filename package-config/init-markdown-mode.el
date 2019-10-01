14bdcf9a71e2fe77c1eb5824f4850de32302a9d7
Skipper Seabold <sseabold@civisanalytics.com>
2019-10-01 09:16:48 -0500

    Style markown faces

--- package-config/init-markdown-mode.el
+++ package-config/init-markdown-mode.el
@@ -4,6 +4,30 @@
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
+  :init (setq markdown-command "multimarkdown")
+  (setq markdown-fontify-code-blocks-natively t)
+  :config
+  (set-face-attribute 'markdown-link-face nil
+                      :foreground "blue")
+  (set-face-attribute 'markdown-header-face nil
+                      :foreground "#dc322f"
+                      :weight 'bold
+                      )
+  (set-face-attribute 'markdown-header-face-1 nil
+                      :inherit markdown-header-face :foreground)
+  (set-face-attribute 'markdown-header-face-2 nil
+                      :inherit markdown-header-face :foreground)
+  (set-face-attribute 'markdown-header-face-3 nil
+                      :inherit markdown-header-face :foreground)
+  (set-face-attribute 'markdown-header-face-4 nil
+                      :inherit markdown-header-face :foreground)
+  (set-face-attribute 'markdown-header-face-5 nil
+                      :inherit markdown-header-face :foreground)
+  (set-face-attribute 'markdown-header-face-6 nil
+                      :inherit markdown-header-face :foreground)
+  (set-face-attribute 'markdown-header-delimiter-face nil
+                      :inherit markdown-header-face :foreground
+            )
+  )
 
 (provide 'init-markdown-mode)
