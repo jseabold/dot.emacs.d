f9acb6707ba78b8492576cad4b10b84989778bfa
Skipper Seabold <jsseabold@gmail.com>
2019-11-01 11:13:34 -0500

    Universal auto-save-list directory

--- init.el
+++ init.el
@@ -67,6 +67,8 @@
 ;; Use default C-c C-e to show eldoc
 (global-eldoc-mode -1)
 
+(setq backup-directory-alist '(("." . "~/.emacs.d/auto-save-list")))
+
 (require 'init-yasnippet)
 (require 'init-linum)
 (require 'init-markdown-mode)
