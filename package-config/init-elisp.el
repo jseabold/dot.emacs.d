476e5adad88d27f32c619a2a5648b2f85fe2fbec
Skipper Seabold <jsseabold@gmail.com>
2019-11-15 13:01:10 -0600

    Suppress closed single quote

--- package-config/init-elisp.el
+++ package-config/init-elisp.el
@@ -1,2 +1,11 @@
+(defun inhibit-closed-quote (char)
+  "Return true if char ' is used"
+  (if (eq char ?') t nil)
+  )
+;; don't close single quotes in emacs-lisp mode
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+             (setq electric-pair-inhibit-predicate 'inhibit-closed-quote)))
+
 (add-hook 'emacs-lisp-mode-hook (lambda() (modify-syntax-entry ?- "w")))
 (provide 'init-elisp)
