17e7bd050867625d7ae7adbcbba42768c8ae7ff2
Skipper Seabold <jsseabold@gmail.com>
2020-05-19 14:31:19 -0500

    Fix parens. Ensure.

--- package-config/init-web-mode.el
+++ package-config/init-web-mode.el
@@ -1,10 +1,11 @@
 (use-package web-mode
-  :mode (
-         ("\\.html\\'" . web-mode)
-         ))
-:config (progn
-          (setq web-mode-markup-indent-offset 2)
-          (setq web-mode-code-indent-offset 2)
-          )
+  :ensure t
+  :mode (("\\.html\\'" . web-mode)
+         )
+  :config (progn
+            (setq web-mode-markup-indent-offset 2)
+            (setq web-mode-code-indent-offset 2)
+            )
+  )
 
 (provide 'init-web-mode)
