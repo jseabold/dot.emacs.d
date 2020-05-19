84a39bd13f5981eb03489237f5008816cb768871
Skipper Seabold <jsseabold@gmail.com>
2020-05-19 10:33:14 -0500

    Fix parens and indentation

--- package-config/init-js2-mode.el
+++ package-config/init-js2-mode.el
@@ -1,13 +1,15 @@
 (use-package js2-mode
   :mode (("\\.js$" . js2-mode)
-         ))
-:config (progn
-  (setq-default js-indent-level 2
-                js-expr-indent-offset -2
-                js-paren-indent-offset 2
-                js2-basic-indent 2
-                js2-basic-offset 2
-                js2-strict-trailing-comma-warning nil)
+         )
+
+  :config (progn
+            (setq-default js-indent-level 2
+                          js-expr-indent-offset -2
+                          js-paren-indent-offset 2
+                          js2-basic-indent 2
+                          js2-basic-offset 2
+                          js2-strict-trailing-comma-warning nil)
+            )
   )
 
 (provide 'init-js2-mode)
--- package-config/init-rjsx-mode.el
+++ package-config/init-rjsx-mode.el
@@ -1,12 +1,14 @@
 (use-package rjsx-mode
   :ensure t
   :mode (("\\.js$" . rjsx-mode)
-         ))
-:config (progn
-          (setq-default js-indent-level 2
-                        js2-basic-indent 2
-                        js2-basic-offset 2
-                        js2-strict-trailing-comma-warning nil
-                        ))
+         )
+  :config (progn
+            (setq-default js-indent-level 2
+                          js2-basic-indent 2
+                          js2-basic-offset 2
+                          js2-strict-trailing-comma-warning nil
+                          )
+            )
+  )
 
 (provide 'init-rjsx-mode)
