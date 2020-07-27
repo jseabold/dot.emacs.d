d521f76338444dcd6a86728285cbfd962767a86f
Skipper Seabold <jsseabold@gmail.com>
2020-07-27 10:59:52 -0500

    Fix error

--- package-config/init-markdown-mode.el
+++ package-config/init-markdown-mode.el
@@ -11,23 +11,30 @@
                       :foreground "blue")
   (set-face-attribute 'markdown-header-face nil
                       :foreground "#dc322f"
+                      :height 1.2
                       :weight 'bold
                       )
   (set-face-attribute 'markdown-header-face-1 nil
-                      :inherit markdown-header-face :foreground)
+                      :inherit 'markdown-header-face
+                      :height 1.15)
   (set-face-attribute 'markdown-header-face-2 nil
-                      :inherit markdown-header-face :foreground)
+                      :inherit 'markdown-header-face
+                      :height 1.10)
   (set-face-attribute 'markdown-header-face-3 nil
-                      :inherit markdown-header-face :foreground)
+                      :inherit 'markdown-header-face
+                      :height 1.05)
   (set-face-attribute 'markdown-header-face-4 nil
-                      :inherit markdown-header-face :foreground)
+                      :inherit 'markdown-header-face
+                      :height 1.025)
   (set-face-attribute 'markdown-header-face-5 nil
-                      :inherit markdown-header-face :foreground)
+                      :inherit 'markdown-header-face
+                      :height 1.0)
   (set-face-attribute 'markdown-header-face-6 nil
-                      :inherit markdown-header-face :foreground)
+                      :inherit 'markdown-header-face
+                      :height 1.0)
   (set-face-attribute 'markdown-header-delimiter-face nil
-                      :inherit markdown-header-face :foreground
-            )
+                      :inherit 'markdown-header-face
+                      :height 1.0)
   )
 
 (provide 'init-markdown-mode)
